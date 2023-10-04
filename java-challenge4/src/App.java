774b195253164adb5f51fd4f292504a35fc7f9a6
Robbie Gollan <robbiegollan@gmail.com>
2023-10-04 12:43:02 +1100

    changes indent size

--- java-challenge4/src/App.java
+++ java-challenge4/src/App.java
@@ -1,9 +1,9 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println(Methods.findArea(3));
-        System.out.println(Methods.fullName("Robbie", "Gollan"));
-        System.out.println(Methods.firstName("Robbie Gollan"));
-        System.out.println(Methods.dateFormatter(59684923));
-        System.out.println(Methods.dataFormatter(2341, 437));
-    }
+  public static void main(String[] args) throws Exception {
+    System.out.println(Methods.findArea(3));
+    System.out.println(Methods.fullName("Robbie", "Gollan"));
+    System.out.println(Methods.firstName("Robbie Gollan"));
+    System.out.println(Methods.dateFormatter(59684923));
+    System.out.println(Methods.dataFormatter(2341, 437));
+  }
 }
